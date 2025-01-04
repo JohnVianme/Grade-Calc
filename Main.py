06906cd70978556e7abac0775b2ef8f9b9d0b785
John Imanishimwe <148114742+JohnVianme@users.noreply.github.com>
2025-01-03 17:35:41 -0700

    Fixed spacing

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from weighted_assignment import weighted_assignment
 def calc_weighted_grade(list_of_weighted_asms):
     score = 0
     for asm in list_of_weighted_asms:
-        score = asm.calc_asm()
+        score += asm.calc_asm()
     return score
 
 
