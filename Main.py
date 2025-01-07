a3cb58c6fe63a5d6b1027b566a350178cadb175d
John Imanishimwe <148114742+JohnVianme@users.noreply.github.com>
2025-01-06 18:24:13 -0700

    updated spaces

--- Main.py
+++ Main.py
@@ -6,60 +6,54 @@ from Points_assignment import points_assignment
 This fuction takes a list of weighted_assignment objects and returns the grade from the 
 the list of assignmets.
 """
+
+
 def calc_weighted_grade(list_of_weighted_asms: List[weighted_assignment]):
     score = 0
     for asm in list_of_weighted_asms:
         score += asm.calc_asm()
     return score
 
+
 """
 This function takes a list of points_assignment object and 
 returns the grade from the list of assignment
 """
 
-def calc_points_grade (list_of_points_asms: List[points_assignment]):
+
+def calc_points_grade(list_of_points_asms: List[points_assignment]):
     score = 0
     tot_points_earned = 0
     tot_points_total = 0
     for asm in list_of_points_asms:
         tot_points_earned += asm.get_points_earned()
         tot_points_total += asm.get_points_total()
-    
+
     score = (tot_points_earned / tot_points_total) * 100
     return score
 
-#def add_assignment ()
+
+# def add_assignment ()
 
 asm_type = input("W-Weighted\nP-Points\n")
 
 
-while (True):
+while True:
     command_ = input("Q-Quit\nD-Display\nO-Overall Grade\nA-Add assignment\nR-Remove\n")
 
-    if (command_ == 'A'):
+    if command_ == "A":
         print("Add element")
-        
-    
-    elif (command_ == 'R'):
+
+    elif command_ == "R":
         print("Remove element")
 
-    elif (command_ == 'D'):
+    elif command_ == "D":
         print("Display")
 
-    elif (command_ == 'O'):
+    elif command_ == "O":
         print("Overall Grade")
-    elif (command_ == 'Q'):
+    elif command_ == "Q":
         print("Exiting...")
         exit()
     else:
         command_ = input("Unknown command, try again\n")
-        
-
-    
-
-    
-
-
-
-
-
