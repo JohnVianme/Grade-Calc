0f370cb087eff09b9e6cf695cecd60e0d87b8d95
John Imanishimwe <148114742+JohnVianme@users.noreply.github.com>
2025-01-08 17:14:35 -0700

    Fix PyQt5 not working

--- weighted_window_GUI.py
+++ weighted_window_GUI.py
@@ -2,18 +2,17 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
-
 def window():
     app = QApplication(sys.argv)
     win = QMainWindow()
-    win.setGeometry(200, 200 ,1080, 720)
+    win.setGeometry(200, 200, 1080, 720)
 
-    label  = QtWidgets.QLabel(win)
+    label = QtWidgets.QLabel(win)
     label.setText("Hello World")
-    label.move(50,50)
+    label.move(50, 50)
 
     win.show()
     sys.exit(app.exec_())
-    
+
+
 window()
-    
