54503aa1bf8edfea387b10f05960de536d5401b6
LXL1103 <mandoan@arizona.edu>
2025-01-10 23:15:03 -0700

    Added_row button

--- points_window_GUI.py
+++ points_window_GUI.py
@@ -25,15 +25,27 @@ window()
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1036, 608)
+        MainWindow.resize(1023, 562)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setMaximumSize(QtCore.QSize(1023, 562))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.splitter = QtWidgets.QSplitter(self.centralwidget)
+        self.splitter.setGeometry(QtCore.QRect(290, 90, 471, 331))  #adjust size of the table
+        self.splitter.setOrientation(QtCore.Qt.Vertical)
+        self.splitter.setObjectName("splitter")
+        self.GradCalcLabel = QtWidgets.QLabel(self.splitter)    
+        self.GradCalcLabel.setObjectName("GradCalcLabel")
+        self.tableWidget = QtWidgets.QTableWidget(self.splitter)
         self.tableWidget.setEnabled(True)
-        self.tableWidget.setGeometry(QtCore.QRect(290, 40, 461, 471))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -42,15 +54,8 @@ class Ui_MainWindow(object):
         self.tableWidget.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustIgnored)
         self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.setColumnCount(3)
-        self.tableWidget.setRowCount(4)
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setVerticalHeaderItem(0, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setVerticalHeaderItem(1, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setVerticalHeaderItem(2, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setVerticalHeaderItem(3, item)
         item = QtWidgets.QTableWidgetItem()
         font = QtGui.QFont()
         font.setBold(True)
@@ -72,19 +77,40 @@ class Ui_MainWindow(object):
         self.tableWidget.horizontalHeader().setDefaultSectionSize(151)
         self.tableWidget.horizontalHeader().setMinimumSectionSize(50)
         self.tableWidget.verticalHeader().setDefaultSectionSize(40)
+        self.addColumn = QtWidgets.QPushButton(self.splitter)
+        self.addColumn.setObjectName("addColumn")
+        self.splitter_3 = QtWidgets.QSplitter(self.centralwidget)
+        self.splitter_3.setGeometry(QtCore.QRect(890, 400, 112, 101))
+        self.splitter_3.setOrientation(QtCore.Qt.Vertical)
+        self.splitter_3.setObjectName("splitter_3")
+        self.splitter_2 = QtWidgets.QSplitter(self.splitter_3)
+        self.splitter_2.setOrientation(QtCore.Qt.Vertical)
+        self.splitter_2.setObjectName("splitter_2")
+        self.label = QtWidgets.QLabel(self.splitter_2)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setUnderline(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.lcdNumber = QtWidgets.QLCDNumber(self.splitter_2)
+        self.lcdNumber.setObjectName("lcdNumber")
+        self.pushButton_2 = QtWidgets.QPushButton(self.splitter_3)
+        self.pushButton_2.setObjectName("pushButton_2")
         MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
         self.toolBar = QtWidgets.QToolBar(MainWindow)
         self.toolBar.setObjectName("toolBar")
         MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
         self.menuBar = QtWidgets.QMenuBar(MainWindow)
-        self.menuBar.setGeometry(QtCore.QRect(0, 0, 1036, 21))
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 1023, 21))
         self.menuBar.setObjectName("menuBar")
         self.menuFile = QtWidgets.QMenu(self.menuBar)
         self.menuFile.setObjectName("menuFile")
         MainWindow.setMenuBar(self.menuBar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
         self.actionSave = QtWidgets.QAction(MainWindow)
         self.actionSave.setObjectName("actionSave")
         self.actionSave_as = QtWidgets.QAction(MainWindow)
@@ -101,18 +127,24 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        self.tableWidget.cellChanged.connect(self.cell_access)
-        self.tableWidget.setItem(2,1, QtWidgets.QTableWidgetItem("100"))
+        self.addColumn.clicked.connect(self.added_column)
+
+        #self.tableWidget.cellChanged.connect(self.cell_access) #sets up a way to get user data 
+        #self.tableWidget.setItem(2,1, QtWidgets.QTableWidgetItem("100")) #sets up a way to output data to GUI
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.GradCalcLabel.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:22pt;\">Grade Calculator</span></p></body></html>"))
         item = self.tableWidget.horizontalHeaderItem(0)
         item.setText(_translate("MainWindow", "Name"))
         item = self.tableWidget.horizontalHeaderItem(1)
         item.setText(_translate("MainWindow", "Points Earned"))
         item = self.tableWidget.horizontalHeaderItem(2)
         item.setText(_translate("MainWindow", "Points Total"))
+        self.addColumn.setText(_translate("MainWindow", "Add Column"))
+        self.label.setText(_translate("MainWindow", "Overall Grade"))
+        self.pushButton_2.setText(_translate("MainWindow", "Click to calculate"))
         self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
         self.menuFile.setTitle(_translate("MainWindow", "File"))
         self.actionSave.setText(_translate("MainWindow", "Save"))
@@ -130,6 +162,20 @@ class Ui_MainWindow(object):
         if cell_content:
             print(f"Content on cell: {cell_content.text()}")
 
+    def added_column(self):
+        rowPosition = self.tableWidget.rowCount()
+        self.tableWidget.insertRow(rowPosition)
+        self.tableWidget.resizeRowsToContents()
+
+        
+        
+
+
+        
+    
+
+    
+
         
 
 
@@ -139,9 +185,7 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
-    
-
-            
+        
     MainWindow.show()       
     sys.exit(app.exec_()) 
         
